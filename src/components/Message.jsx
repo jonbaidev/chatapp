15dfb719dd6c3e9785a71b3591eb647192d437e4
jonbaidev <jonathanbaideveloper@gmail.com>
2025-02-18 20:33:43 -0300

    add sidebar and chat component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className="message">
+      <div className="messageInfo">
+        <img src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        {/* <img src="https://images.pexels.com/photos/1181690/pexels-photo-1181690.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" /> */}
+      </div>
+    </div>
   )
 }
 
