bfd55351d8587e6929b0c32d9f4b2055fadae150
jonbaidev <jonathanbaideveloper@gmail.com>
2025-02-18 14:25:46 -0300

    add Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
+      <div className="formContainer">
+          <div className="formWrapper">
+              <span className="logo">JonBai Chat</span>
+              <span className="title">Login</span>
+              <form action="">
+                  <input type="email" placeholder="email"/>
+                  <input type="password" placeholder="password"/>
+                  <button>Sign In</button>
+              </form>
+              <p>You don't have an account? Register</p>
+          </div>
+      </div>
+    )
 }
 
 export default Login
